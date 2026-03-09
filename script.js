6784ae762d19c3af01d9a60fae2c6a0c0219d8af
manoj9900 <133666206+manoj9900@users.noreply.github.com>
2026-03-09 18:15:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const header = document.querySelector('.header-wrapper');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 80) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+
+
+
+
+
+// TOGGLE MOBILE MENU
+const mobileToggle = document.getElementById('mobileToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+mobileToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+    mobileToggle.classList.toggle('active'); // triggers hamburger → X
+});
+
+
+
+
+
+
+
+
+
+
+
+
